438fb36982a6388d5afd44e40eee70abf029d287
chenhonghao12 <chenhonghao12@jd.com>
2024-11-05 15:05:28 +0800

    feat(RuleEngineAutoConfiguration): 添加 MyBatis Plus 扫描注解
    - 在 RuleEngineAutoConfiguration 类中添加了 @MapperScan 注解
    - 指定 MyBatis Plus 的 Mapper 接口扫描包路径为 "com.jd.cho.rule.engine.infr.customer"

--- rule-engine-starter/src/main/java/com/jd/cho/rule/engine/starter/RuleEngineAutoConfiguration.java
+++ rule-engine-starter/src/main/java/com/jd/cho/rule/engine/starter/RuleEngineAutoConfiguration.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ComponentScan("com.jd.cho.rule.engine")
+
 @MapperScan({"com.jd.cho.rule.engine.infr.customer"})
 public class RuleEngineAutoConfiguration {
 
