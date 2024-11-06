1abd060e5ca856cd30cebf5574a22d337e61d6a9
chenhonghao12 <chenhonghao12@jd.com>
2024-11-06 14:33:29 +0800

    refactor(rule-engine-app): 重构规则场景服务实现类
    - 添加 @Service 注解，标识为 Spring 服务类
    - 删除未实现的 queryBySceneCode 方法

--- rule-engine-app/src/main/java/com/jd/cho/rule/engine/app/service/RuleSceneServiceImpl.java
+++ rule-engine-app/src/main/java/com/jd/cho/rule/engine/app/service/RuleSceneServiceImpl.java
@@ -2,21 +2,17 @@ package com.jd.cho.rule.engine.app.service;
 
 import com.jd.cho.rule.engine.client.api.RuleSceneService;
 import com.jd.cho.rule.engine.client.dto.RuleSceneDTO;
-
-import java.util.List;
+import org.springframework.stereotype.Service;
 
 /**
  * @author chenhonghao12
  * @version 1.0
  */
+@Service
 public class RuleSceneServiceImpl implements RuleSceneService {
     @Override
     public String createScene(RuleSceneDTO ruleSceneDTO) {
         return null;
     }
 
-    @Override
-    public List<RuleSceneDTO> queryBySceneCode(String sceneCode) {
-        return null;
-    }
 }
