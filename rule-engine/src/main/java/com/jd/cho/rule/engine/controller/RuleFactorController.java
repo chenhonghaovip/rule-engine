5403264385209f79e185d5db2310c9445b434d9e
chenhonghao12 <chenhonghao12@jd.com>
2024-11-07 18:25:55 +0800

    feat(rule-engine): 新增规则引擎相关功能
    - 添加 AtomicLoginUserComponent 类用于获取当前登录用户信息
    - 新增 LoginUserService 接口及实现类
    - 修改 RuleConfigGateway 接口，添加批量创建规则方法
    - 实现 RuleConfigGatewayImpl 类中的批量创建规则逻辑
    - 添加 RuleDefDTO 和 RuleDefQueryDTO 类用于规则定义
    - 新增 RuleFactor、RuleFactorGroup 和 RuleScene 相关控制器和请求类
    - 实现部分规则引擎核心功能，如规则创建、修改等

--- rule-engine/src/main/java/com/jd/cho/rule/engine/controller/RuleFactorController.java
+++ rule-engine/src/main/java/com/jd/cho/rule/engine/controller/RuleFactorController.java
@@ -2,12 +2,10 @@ package com.jd.cho.rule.engine.controller;
 
 import com.jd.cho.rule.engine.controller.VO.req.RuleFactorReq;
 import com.jd.cho.rule.engine.service.RuleFactorService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * 规则引擎规则场景控制中心
@@ -22,26 +20,24 @@ public class RuleFactorController {
     private RuleFactorService ruleFactorService;
 
     /**
-     * 获取规则场景列表信息
+     * 获取规则因子列表
      *
      * @return list
      */
     @GetMapping(value = "/list")
-    public String list() {
+    public List<RuleFactorReq> list(@RequestParam("groupCode") String groupCode) {
 
         return null;
     }
 
 
     @PostMapping(value = "/create")
-    public String create(RuleFactorReq ruleFactorReq) {
-        return null;
+    public void create(RuleFactorReq ruleFactorReq) {
     }
 
 
     @PostMapping(value = "/modify")
-    public String modify(RuleFactorReq ruleFactorReq) {
-        return null;
+    public void modify(RuleFactorReq ruleFactorReq) {
     }
 
 }
