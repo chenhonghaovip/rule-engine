cf2072c4209ba0bdb4b01499dca10b8724692ec3
liweichao11 <liweichao3@jd.com>
2024-11-29 19:49:51 +0800

    test: 运行 factor 的表达式

---
+++ rule-engine-runtime/src/test/java/com/jd/cho/rule/engine/core/executer/CoreDecisionSetRuleExecutor_ConstTest.java
@@ -0,0 +1,167 @@
+package com.jd.cho.rule.engine.core.executer;
+
+import com.jd.cho.rule.engine.common.dict.Dict;
+import com.jd.cho.rule.engine.common.enums.ActionType;
+import com.jd.cho.rule.engine.common.enums.ExpressOperationEnum;
+import com.jd.cho.rule.engine.common.enums.VarTypeEnum;
+import com.jd.cho.rule.engine.common.util.ApplicationUtils;
+import com.jd.cho.rule.engine.domain.atomic.FactorValueService;
+import com.jd.cho.rule.engine.domain.atomic.impl.FactorValueServiceImpl;
+import com.jd.cho.rule.engine.domain.gateway.RuleConfigGateway;
+import com.jd.cho.rule.engine.domain.model.BasicVar;
+import com.jd.cho.rule.engine.domain.model.RuleAction;
+import com.jd.cho.rule.engine.domain.model.RuleCondition;
+import com.jd.cho.rule.engine.domain.model.RuleDef;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+class CoreDecisionSetRuleExecutor_ConstTest {
+    private RuleConfigGateway ruleConfigGateway;
+    private CoreDecisionSetRuleExecutor executor;
+    private FactorValueService factorValueService;
+    private MockedStatic<ApplicationUtils> staticApplicationUtils;
+
+    @BeforeEach
+    void setUp() {
+        ruleConfigGateway = Mockito.mock(RuleConfigGateway.class);
+        executor = new CoreDecisionSetRuleExecutor(ruleConfigGateway);
+        factorValueService = new FactorValueServiceImpl(ruleConfigGateway);
+        staticApplicationUtils = Mockito.mockStatic(ApplicationUtils.class);
+        staticApplicationUtils.when(() -> ApplicationUtils.getBeans(FactorValueService.class)).thenReturn(factorValueService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        staticApplicationUtils.close();
+    }
+
+    @DisplayName("condition(single expr): 1 = 1")
+    @Test
+    void test_const_eq_const() {
+        final Integer constValue = 1;
+        final RuleDef ruleDef = new RuleDef() {{
+            this.setPriority(1);
+            this.setRuleCondition(new RuleCondition() {{
+                this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
+                this.setLeftVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+                this.setRightVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+            }});
+        }};
+
+        final Map<String, Object> context = new HashMap<>();
+        boolean matched = executor.execute(ruleDef, context);
+        Assertions.assertThat(matched).isTrue();
+    }
+
+    @DisplayName("condition(single expr): not 1 = 2")
+    @Test
+    void test_not_const_eq_const() {
+        final Integer constOne = 1;
+        final Integer constTwo = 2;
+        final RuleDef ruleDef = new RuleDef() {{
+            this.setPriority(1);
+            this.setRuleCondition(new RuleCondition() {{
+                this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
+                this.setLeftVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constOne);
+                }});
+                this.setRightVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constTwo);
+                }});
+            }});
+        }};
+
+        final Map<String, Object> context = new HashMap<>();
+        boolean matched = executor.execute(ruleDef, context);
+        Assertions.assertThat(matched).isFalse();
+    }
+
+    @DisplayName("condition(single expr): 1 = 1, action=assign('resultNum',10)")
+    @Test
+    void test_const_eq_const_and_setResult() {
+        final Integer constValue = 1;
+        final String resultNumKey = "resultNum";
+        final RuleDef ruleDef = new RuleDef() {{
+            this.setPriority(1);
+            this.setRuleCondition(new RuleCondition() {{
+                this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
+                this.setLeftVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+                this.setRightVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+            }});
+            this.setRuleActions(new ArrayList<RuleAction>() {{
+                this.add(new RuleAction() {{
+                    this.setExecuteType(ActionType.ASSIGN.getCode());
+                    this.setFieldCode(resultNumKey);
+                    this.setValues(10);
+                }});
+            }});
+        }};
+
+        final Map<String, Object> context = new HashMap<>();
+        boolean matched = executor.execute(ruleDef, context);
+        Assertions.assertThat(matched).isTrue();
+        Map<String, Object> result = (Map<String, Object>) context.get(Dict.RESULT_ALIAS);
+        Assertions.assertThat(result).containsEntry(resultNumKey, 10);
+    }
+
+    @DisplayName("condition(single expr): 1 = 1, initResult, action=assign('resultNum',10)")
+    @Test
+    void test_const_eq_const_and_initResult_setResult() {
+        final Integer constValue = 1;
+        final String resultNumKey = "resultNum";
+        final RuleDef ruleDef = new RuleDef() {{
+            this.setPriority(1);
+            this.setRuleCondition(new RuleCondition() {{
+                this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
+                this.setLeftVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+                this.setRightVar(new BasicVar() {{
+                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
+                    this.setValues(constValue);
+                }});
+            }});
+            this.setRuleActions(new ArrayList<RuleAction>() {{
+                this.add(new RuleAction() {{
+                    this.setExecuteType(ActionType.ASSIGN.getCode());
+                    this.setFieldCode(resultNumKey);
+                    this.setValues(10);
+                }});
+            }});
+        }};
+
+        final Map<String, Object> context = new HashMap<>();
+        context.put(Dict.RESULT_ALIAS, new HashMap<String, Object>() {{
+            this.put("a", "A");
+        }});
+        boolean matched = executor.execute(ruleDef, context);
+        Assertions.assertThat(matched).isTrue();
+        Map<String, Object> result = (Map<String, Object>) context.get(Dict.RESULT_ALIAS);
+        Assertions.assertThat(result).containsEntry(resultNumKey, 10);
+        Assertions.assertThat(result).containsEntry("a", "A");
+    }
+}
--- rule-engine-runtime/src/test/java/com/jd/cho/rule/engine/core/executer/CoreDecisionSetRuleExecutor_FactorTest.java
+++ rule-engine-runtime/src/test/java/com/jd/cho/rule/engine/core/executer/CoreDecisionSetRuleExecutor_FactorTest.java
@@ -1,6 +1,5 @@
 package com.jd.cho.rule.engine.core.executer;
 
-import com.jd.cho.rule.engine.common.enums.ActionType;
 import com.jd.cho.rule.engine.common.enums.ExpressOperationEnum;
 import com.jd.cho.rule.engine.common.enums.VarTypeEnum;
 import com.jd.cho.rule.engine.common.util.ApplicationUtils;
@@ -8,9 +7,9 @@ import com.jd.cho.rule.engine.domain.atomic.FactorValueService;
 import com.jd.cho.rule.engine.domain.atomic.impl.FactorValueServiceImpl;
 import com.jd.cho.rule.engine.domain.gateway.RuleConfigGateway;
 import com.jd.cho.rule.engine.domain.model.BasicVar;
-import com.jd.cho.rule.engine.domain.model.RuleAction;
 import com.jd.cho.rule.engine.domain.model.RuleCondition;
 import com.jd.cho.rule.engine.domain.model.RuleDef;
+import com.jd.cho.rule.engine.domain.model.RuleFactor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,7 +22,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-class CoreDecisionSetRuleExecutorTest {
+public class CoreDecisionSetRuleExecutor_FactorTest {
     private RuleConfigGateway ruleConfigGateway;
     private CoreDecisionSetRuleExecutor executor;
     private FactorValueService factorValueService;
@@ -43,60 +42,65 @@ class CoreDecisionSetRuleExecutorTest {
         staticApplicationUtils.close();
     }
 
-    @DisplayName("condition(single expr): 1 = 1")
+    @DisplayName("condition(single expr): factorOne = 1")
     @Test
-    void test_const_eq_const() {
-        final Integer constValue = 1;
+    void test_factor_eq_const() {
+        final String factorOne = "factorOne";
+        final Integer constOne = 1;
         final RuleDef ruleDef = new RuleDef() {{
             this.setPriority(1);
             this.setRuleCondition(new RuleCondition() {{
                 this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
                 this.setLeftVar(new BasicVar() {{
-                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
-                    this.setValues(constValue);
+                    this.setRuleType(VarTypeEnum.FACTOR.getCode());
+                    this.setCode(factorOne + "Code");
+                    this.setOriginalFactorCode(factorOne);
                 }});
                 this.setRightVar(new BasicVar() {{
                     this.setRuleType(VarTypeEnum.CONSTANT.getCode());
-                    this.setValues(constValue);
+                    this.setValues(constOne);
                 }});
             }});
         }};
+
+        Mockito.when(ruleConfigGateway.queryFactorCodes()).thenReturn(new ArrayList<RuleFactor>() {{
+            this.add(RuleFactor.builder().factorCode(factorOne).factorScript("return 1;").build());
+        }});
 
         final Map<String, Object> context = new HashMap<>();
         boolean matched = executor.execute(ruleDef, context);
         Assertions.assertThat(matched).isTrue();
     }
 
-    @DisplayName("condition(single expr): 1 = 1, action=assign('resultNum',10)")
+    @DisplayName("condition(single expr): factorOne < factorTwo")
     @Test
-    void test_const_eq_const_and_setResult() {
-        final Integer constValue = 1;
+    void test_factor_eq_factor() {
+        final String factorOne = "factorOne";
+        final String factorTwo = "factorTwo";
         final RuleDef ruleDef = new RuleDef() {{
             this.setPriority(1);
             this.setRuleCondition(new RuleCondition() {{
-                this.setCompareOperation(ExpressOperationEnum.NUM_EQUAL.getOperator());
+                this.setCompareOperation(ExpressOperationEnum.NUM_LESS_THAN.getOperator());
                 this.setLeftVar(new BasicVar() {{
-                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
-                    this.setValues(constValue);
+                    this.setRuleType(VarTypeEnum.FACTOR.getCode());
+                    this.setCode(factorOne + "Code");
+                    this.setOriginalFactorCode(factorOne);
                 }});
                 this.setRightVar(new BasicVar() {{
-                    this.setRuleType(VarTypeEnum.CONSTANT.getCode());
-                    this.setValues(constValue);
-                }});
-            }});
-            this.setRuleActions(new ArrayList<RuleAction>() {{
-                this.add(new RuleAction() {{
-                    this.setExecuteType(ActionType.ASSIGN.getCode());
-                    this.setFieldCode("resultNum");
-                    this.setValues(10);
+                    this.setRuleType(VarTypeEnum.FACTOR.getCode());
+                    this.setCode(factorTwo + "Code");
+                    this.setOriginalFactorCode(factorTwo);
                 }});
             }});
         }};
 
+        Mockito.when(ruleConfigGateway.queryFactorCodes()).thenReturn(new ArrayList<RuleFactor>() {{
+            this.add(RuleFactor.builder().factorCode(factorOne).factorScript("return 1;").build());
+            this.add(RuleFactor.builder().factorCode(factorTwo).factorScript("return 2;").build());
+        }});
+
         final Map<String, Object> context = new HashMap<>();
         boolean matched = executor.execute(ruleDef, context);
         Assertions.assertThat(matched).isTrue();
-        Map<String, Object> result = (Map<String, Object>) context.get("result");
-        Assertions.assertThat(result).containsEntry("resultNum", 10);
     }
 }
